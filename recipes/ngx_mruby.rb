8972872a6af69a6fc5ab324238514058e9063247
hirocaster <hohtsuka@gmail.com>
2015-07-30 22:08:24 +0900

    Fix build ngx_mruby

--- recipes/ngx_mruby.rb
+++ recipes/ngx_mruby.rb
@@ -4,10 +4,15 @@
 include_recipe 'mruby::default'
 
 git ::File.join(node[:mruby][:build_dir],'ngx_mruby') do
-  action :sync
+  action :checkout
   reference node[:mruby][:ngx_mruby][:git_refernce]
   repository 'https://github.com/matsumoto-r/ngx_mruby.git'
   enable_submodules true
+end
+
+template 'ngx_mruby_builld_config' do
+  source 'ngx_build_config.erb'
+  path "#{::File.join(node[:mruby][:build_dir],'ngx_mruby/build_config.rb')}"
 end
 
 bash 'sync_built_mruby' do
@@ -16,6 +21,12 @@ bash 'sync_built_mruby' do
   EOL
 end
 
+nginx_src_dir_path  = "#{Chef::Config['file_cache_path'] || '/tmp'}/nginx-#{node['nginx']['source']['version']}"
+
+execute 'ngx_mruby configure, build_mruby, generate_gems_config' do
+  cwd File.join(node[:mruby][:build_dir],'ngx_mruby')
+  command "./configure --with-ngx-src-root=#{nginx_src_dir_path} && make build_mruby && make generate_gems_config"
+end
 
 ngx_mruby_modules = [
   "--add-module=#{::File.join(node[:mruby][:build_dir],'ngx_mruby')}",
@@ -25,4 +36,3 @@ ngx_mruby_modules = [
 node.run_state['nginx_configure_flags'] = [] unless node.run_state['nginx_configure_flags']
 node.run_state['nginx_configure_flags'] =
   node.run_state['nginx_configure_flags'] | ngx_mruby_modules
-
